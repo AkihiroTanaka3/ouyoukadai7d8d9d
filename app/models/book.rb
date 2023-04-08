d186885d9f7a68b1acf3fcee51dc278373973fe2
AkihiroTanaka3 <qqqwwetyu@gmail.com>
2023-04-08 02:54:19 +0000

    [finish]8b

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,21 +4,26 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorites_users, through: :favorites, source: :user
   has_many :view_counts, dependent: :destroy
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   # scope :スコープの名前, -> { 条件式 }でメソッド化できる
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) } #今日
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } #昨日
+  scope :created_2days, -> { where(created_at: 2.day.ago.all_day) } #２日前
+  scope :created_3days, -> { where(created_at: 3.day.ago.all_day) } #３日前
+  scope :created_4days, -> { where(created_at: 4.day.ago.all_day) } #４日前
+  scope :created_5days, -> { where(created_at: 5.day.ago.all_day) } #５日前
+  scope :created_6days, -> { where(created_at: 6.day.ago.all_day) } #６日前
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } #今週
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } #先週
-  
-  
+
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def self.looks(search, word)
     if search == "perfect_match"
       @book = Book.where("title LIKE?","#{word}")
